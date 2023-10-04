6110a59f473ed24a3ee696f5aa4a5dcee68d53d6
jmessina <j.w.messina24@gmail.com>
2023-10-04 16:04:34 -0700

    Implemented scrolling feature on home page

--- components/auth/navigation/screens/Home.js
+++ components/auth/navigation/screens/Home.js
@@ -1,21 +1,35 @@
 //Avlokita's Work
 
 import * as React from 'react'
-import { StyleSheet, View, Text, Image } from "react-native"
+import { StyleSheet, View, Text, Image, SafeAreaView, StatusBar, VirtualizedList} from "react-native"
 
 
 export default function Home({navigation}){
     return(
-        <View style = {styles.container}>
-            <Image source={require('../../../../assets/background.png')}/>
-        </View>
+        <SafeAreaView style={styles.scroll_container}>
+        <VirtualizedList
+            initialNumToRender={8}
+            renderItem={({item}) => <Item title={item.title} />}
+            keyExtractor={item => item.id}
+            getItemCount={getItemCount}
+            getItem={getItem}
+             />
+        </SafeAreaView>
     )
-        /* <View style= {styles.container}w>
-            <Text style={styles.temp} >
-                Hello!                      
-            </Text>   
-        </View> */
 }
+const getItem = (_data, index) => ({
+    id: Math.random().toString(12).substring(0),
+    title: `Item ${index + 1}`,
+  });
+  
+const getItemCount = _data => 50;
+
+const Item = ({title}) => (
+<View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+</View>
+);
+
 
 const styles = StyleSheet.create({
     container: {
@@ -30,5 +44,19 @@ const styles = StyleSheet.create({
         color: 'blue',
     },
 
-
+    scroll_container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight,
+      },
+      item: {
+        backgroundColor: '#f9c2ff',
+        height: 150,
+        justifyContent: 'center',
+        marginVertical: 8,
+        marginHorizontal: 16,
+        padding: 20,
+      },
+      title: {
+        fontSize: 32,
+      },
   });
