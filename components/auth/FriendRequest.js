86979d5032d7f87bfb53dc7a30b801d4887e80eb
jmessina <j.w.messina24@gmail.com>
2023-12-07 13:15:43 -0800

    Small merge, just adding filenames to directory

---
+++ components/auth/FriendRequest.js
@@ -0,0 +1,34 @@
+// FriendRequest.js
+
+// import React, { useState } from 'react';
+// import { View, Text, Button, StyleSheet, Alert } from 'react-native';
+
+// const FriendRequest = () => {
+//   const [friendName, setFriendName] = useState('John Doe'); // You can replace this with dynamic data
+
+//   const sendFriendRequest = () => {
+//     const message = `Friend request sent to ${friendName}!`;
+//     Alert.alert('Friend Request', message);
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.friendName}>{friendName}</Text>
+//       <Button title="Send Friend Request" onPress={sendFriendRequest} />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   friendName: {
+//     fontSize: 18,
+//     marginBottom: 20,
+//   },
+// });
+
+// export default FriendRequest;
